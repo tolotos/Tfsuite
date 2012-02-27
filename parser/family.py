3e2e5ea78ff5f511d8bb59c1bfd149521536ef25
Fabian Zimmer <fbn.zimmer@gmail.com>
2012-02-27 11:09:38 +0100

    Added proper exit for the family.py parser

--- parser/family.py
+++ parser/family.py
@@ -4,18 +4,24 @@
 #       fasta.py
 
 import os
+import sys
+
 
 def read_family(source, obj=None):
     family2arrangement = {}
 
     try:
-        basename = os.path.basename(source)
+        #basename = os.path.basename(source)
 
         with open(source, "r") as file:
             for line in file.readlines():
                 line = line.split()
                 family2arrangement[line[0]] = line[1]
             return family2arrangement
-    
+
     except IOError:
-        print "File does not exit!"
+        print "!----ERROR----!"
+        print "File %s does not exit!" % source
+        sys.exit(1)
+    except KeyboardInterrupt:
+        sys.exit(1)
